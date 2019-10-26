25a0766056daa05bb1dffe584de29239fd29e1f8
Flambard alan <alanfla@hotmail.fr>
2019-10-26 18:03:49 +0200

    Update upgrade_auto.sh

--- upgrade_auto.sh
+++ upgrade_auto.sh
@@ -4,12 +4,12 @@
 pass="$1";
 
 echo "Mise a jour des dépôt"
-echo $pass | sudo -S apt update
+echo $pass | sudo -S apt update -y
 echo ""
 echo "Mise a jour des paquet"
-echo $pass | sudo -S apt full-upgrade
+echo $pass | sudo -S apt full-upgrade -y
 echo ""
 echo "Verification des paquet orphelin et suppression"
-echo $pass | sudo -S apt autoremove
+echo $pass | sudo -S apt autoremove -y
 echo ""
 echo "Mise a jour du système terminer"
